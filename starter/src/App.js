56b50b9c09aa4f4d580c64fc27c611a3e6318aaa
chaudhuree <chaudhuree00@gmail.com>
2022-07-27 00:40:11 +0600

    docs: modal setup done

--- starter/src/App.js
+++ starter/src/App.js
@@ -2,14 +2,17 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
+import Modal from "./components/Modal";
 import { calculateTotal } from './features/cart/cartSlice';
 function App() {
   const {cartItems}=useSelector((store)=>store.cart)
+  const {isOpen}=useSelector((store)=>store.modal)
   const dispatch=useDispatch()
   useEffect(()=>{
     dispatch(calculateTotal())
-  },[cartItems])
+  },[cartItems,dispatch]);
   return <main>
+  {isOpen && <Modal></Modal>}
     <Navbar></Navbar>
     <CartContainer></CartContainer>
   </main>
